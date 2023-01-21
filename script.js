4699ff5d50053a2be25e2e55c7c9cf6bb74be16b
Patrick Brebner <patrick.brebner@gmail.com>
2023-01-20 19:00:07 -0800

    Add basic calculator functions

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 // Script for Calculator Project
+
+function add(num1, num2) {
+    return num1 + num2
+}
+
+function subtract(num1, num2) {
+    return num1 - num2
+}
+
+function multiply(num1, num2) {
+    return num1 * num2
+}
+
+function divide(num1, num2) {
+    return num1 / num2
+}
+
+function operate(operator, num1, num2){
+    if (operator == 'add') {
+        add(num1, num2)
+    }
+    else if (operator == 'subtract') {
+        subtract(num1, num2)
+    }
+    else if (operator == 'multiply') {
+        multiply(num1, num2)
+    }
+    else {
+        divide(num1, num2)
+    }
+}
